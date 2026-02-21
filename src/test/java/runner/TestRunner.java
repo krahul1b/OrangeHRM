7980c48fe4dcb371d4cd6502cd718cc27443fc31
krahul1b <krahul1b@gmail.com>
2026-02-21 16:48:21 +0530

    Code enhancement

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,12 +9,7 @@ import org.testng.annotations.DataProvider;
         tags = "(@smoke or @regression) and not (@wip or @ignore)")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
-    @Override
-    @DataProvider(parallel = true)
-    public Object[][] scenarios()
-    {
-        return super.scenarios();
-    }
+
 }
 
 
