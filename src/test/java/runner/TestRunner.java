ab79668f3964d801236cc87b53ac8500d396bc25
krahul1b <krahul1b@gmail.com>
2026-02-21 17:03:46 +0530

    Code enhancement

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,7 +1,6 @@
 package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/resources/features",
         glue = {"stepdefinitions","browser"},
